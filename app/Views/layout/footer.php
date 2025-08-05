b8347b9b8fbdb809f07b4ddf84f0c209f19973ca
Abhishek Bhardwaj <40014349+abhiib02@users.noreply.github.com>
2025-08-05 21:51:50 +0530

    Deploy: Un-commented footer section for author attribution

--- app/Views/layout/footer.php
+++ app/Views/layout/footer.php
@@ -10,9 +10,9 @@
   </script>
 <?php endif; ?>
 
-<!-- <div class="position-fixed opacity-25" style="bottom:0px;right:0px;">
+<div class="position-fixed opacity-25" style="bottom:0px;right:0px;">
   <a href="https://bhardwaj.netlify.app/" target="_blank"><span class="badge text-bg-light"> Made By Abhishek Bhardwaj</span></a>
-</div> -->
+</div>
 
 </body>
 
