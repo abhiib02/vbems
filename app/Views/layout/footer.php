5f1e3200861fcc81081ec2786dfd7f144753f894
Abhishek Bhardwaj <40014349+abhiib02@users.noreply.github.com>
2025-08-05 10:38:54 +0530

    Deploy: 5 Aug 2025

--- app/Views/layout/footer.php
+++ app/Views/layout/footer.php
@@ -9,11 +9,11 @@
     runToast(<?= json_encode($flash['message']) ?>, <?= json_encode($flash['status']) ?>);
   </script>
 <?php endif; ?>
-<!--
+
 <div class="position-fixed opacity-25" style="bottom:0px;right:0px;">
   <a href="https://bhardwaj.netlify.app/" target="_blank"><span class="badge text-bg-light"> Made By Abhishek Bhardwaj</span></a>
 </div>
--->
+
 </body>
 
 </html>
