3851ceb87255df5a480a1ac4dce91bc64f723b03
Abhishek Bhardwaj <40014349+abhiib02@users.noreply.github.com>
2025-08-06 11:02:05 +0530

    Deploy: Implement code changes to enhance functionality and improve performance

--- app/Views/layout/components/stylesheets.php
+++ app/Views/layout/components/stylesheets.php
@@ -1,10 +1,9 @@
 <link rel="preconnect" href="https://fonts.googleapis.com">
 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
 <link href="https://fonts.googleapis.com/css2?family=Roboto+Condensed:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
-<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/css/bootstrap.min.css" rel="stylesheet">
-
-<link rel="stylesheet" href="/css/remixicon.css">
-<link rel="stylesheet" href="/css/dashboard.css">
-<link rel="stylesheet" href="/css/calender.css">
-<link rel="stylesheet" href="/css/dialog.css">
-<link rel="stylesheet" href="/css/style.css">
+<link href="/css/bootstrap5.min.css?v=1" rel="stylesheet">
+<link rel="stylesheet" href="/css/remixicon.css?v=1">
+<link rel="stylesheet" href="/css/dashboard.css?v=1">
+<link rel="stylesheet" href="/css/calender.css?v=1">
+<link rel="stylesheet" href="/css/dialog.css?v=1">
+<link rel="stylesheet" href="/css/style.css?v=1">
